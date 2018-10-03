c99e38dffa160bd8ef1b577f7ebc0e6a28cf69d4
sajir khenissi <h-sk.sajir@hotmail.fr>
2018-10-03 19:05:44 +0200

    modal show onload + button hide

---
+++ public/javascripts/script.js
@@ -0,0 +1,34 @@
+// $('#myButton').click(function(){
+//     var dropdown1 = $('#niveauEtude').value();
+//     var dropdown2 = $('#typeContract').value();
+//     var dropdown3 = $('#secteur').value();
+//     var dropdown4 = $('#salaire').value();
+//     if (dropdown1=='Bac') {
+//       $('#modal1').show();
+//       $('#modal2').hide();
+//       $('#modal3').hide();
+//     }
+//     if (dropdown1=='BTS') {
+//       $('#modal1').hide();
+//       $('#modal2').show();
+//       $('#modal3').hide();
+//     }
+//     if (dropdown1=='uvkhsrtsvh' && dropdown2=='geuriohrg' && dropdown3=='eroihhe') {
+//       $('#modal1').hide();
+//       $('#modal2').hide();
+//       $('#modal3').show();
+//     }
+//   });
+
+// ***************** afficher la modal du jeu au build *********************************
+$("#modal1").click(function() {
+    $("#GameModalButton").show();
+  });
+  $( document ).ready(function() {
+    $("#ModalJeu").modal('show');
+    $("#GameModalButton").hide();
+
+
+
+      // $("#GameModalButton").show();
+  });
